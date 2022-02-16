a244c28790379bb80e8da12e14f1951de4337304
Babymu21 <99784792+Babymu21@users.noreply.github.com>
2022-02-16 21:53:27 +0700

    Update salam.py

--- userbot/modules/salam.py
+++ userbot/modules/salam.py
@@ -9,7 +9,7 @@ from userbot.utils import edit_or_reply, poci_cmd
 async def _(event):
     await event.client.send_message(
         event.chat_id,
-        "**Assalamualaikum Dulu Biar Sopan**",
+        "**Assalamualaikum Dulu Biar Sopan ngentod**",
         reply_to=event.reply_to_msg_id,
     )
     await event.delete()
