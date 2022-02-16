a9251403591de02bc25ae53f37a832976516b86c
Babymu21 <99784792+Babymu21@users.noreply.github.com>
2022-02-16 18:26:33 +0700

    Update ping.py

--- userbot/modules/ping.py
+++ userbot/modules/ping.py
@@ -55,7 +55,7 @@ async def _(ping):
     """For .ping command, ping the userbot from any chat."""
     uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
-    xx = await edit_or_reply(ping, "**⚡**")
+    xx = await edit_or_reply(ping, "**😡**")
     await xx.edit("**P**")
     await xx.edit("**Po**")
     await xx.edit("**Pon**")
