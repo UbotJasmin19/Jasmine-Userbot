8975ec077ab7a36d0be67d57cf8c8fbc0656cc23
Babymu21 <99784792+Babymu21@users.noreply.github.com>
2022-03-03 11:48:55 +0700

    Update asupan.py

--- userbot/modules/asupan.py
+++ userbot/modules/asupan.py
@@ -1,52 +1,42 @@
 # 🍀 © @tofik_dn
 # ⚠️ Do not remove credits
 
-import requests
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
 from userbot.utils import poci_cmd
+import random
+from userbot import owner
+from telethon.tl.types import InputMessagesFilterVideo
+from telethon.tl.types import InputMessagesFilterVoice
 
 
 @poci_cmd(pattern="asupan$")
 async def _(event):
     try:
-        response = requests.get("https://api-alphabot.herokuapp.com/api/asupan/chika?apikey=Alphabot").json()
-        await event.client.send_file(event.chat_id, response["url"])
+        asupannya = [
+            asupan
+            async for asupan in event.client.iter_messages(
+                "@Gabutnyazaen", filter=InputMessagesFilterVideo
+            )
+        ]
+        aing = await event.client.get_me()
+        await event.client.send_file(
+            event.chat_id,
+            file=random.choice(asupannya),
+            caption=f"**Berhasil menemukan Video**.")
+        )
         await event.delete()
     except Exception:
-        await event.edit("**Tidak bisa menemukan video asupan.**")
+        await event.edit("Tidak bisa menemukan video asupan.")
 
-
-@poci_cmd(pattern="wibu$")
-async def _(event):
-    try:
-        response = requests.get("https://api-tede.herokuapp.com/api/asupan/wibu").json()
-        await event.client.send_file(event.chat_id, response["url"])
-        await event.delete()
-    except Exception:
-        await event.edit("**Tidak bisa menemukan video wibu.**")
-
-
-@poci_cmd(pattern="chika$")
-async def _(event):
-    try:
-        response = requests.get("https://api-alphabot.herokuapp.com/api/asupan/chika?apikey=Alphabot").json()
-        await event.client.send_file(event.chat_id, response["url"])
-        await event.delete()
-    except Exception:
-        await event.edit("**Tidak bisa menemukan video chikakiku.**")
-
+        
 
 CMD_HELP.update(
     {
         "asupan": f"**Plugin : **`asupan`\
         \n\n  •  **Syntax :** `{cmd}asupan`\
         \n  •  **Function : **Untuk Mengirim video asupan secara random.\
-        \n\n  •  **Syntax :** `{cmd}wibu`\
-        \n  •  **Function : **Untuk Mengirim video wibu secara random.\
-        \n\n  •  **Syntax :** `{cmd}chika`\
-        \n  •  **Function : **Untuk Mengirim video chikakiku secara random.\
     "
     }
 )
