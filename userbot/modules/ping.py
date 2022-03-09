bb0c4b1dba8302256adba1c7a78e4c25782339d8
Babymu21 <99784792+Babymu21@users.noreply.github.com>
2022-03-09 21:33:40 +0700

    Update ping.py

--- userbot/modules/ping.py
+++ userbot/modules/ping.py
@@ -120,7 +120,7 @@ async def _(pong):
 #  .Coded by Ramadhani RAM-UBOT
 
 @poci_cmd(pattern="tping$")
-async def_(pong):
+async def _(pong):
     """ For .ping command, ping the userbot from any chat.  """
     await get_readable_time((time.time() - StartTime))
     start = datetime.now()
